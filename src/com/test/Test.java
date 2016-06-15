5eca7c19a2ed13136c5357a35965ca5bfeba9166
oyang414 <o_yang1993@163.com>
2016-06-15 09:51:01 +0800

    第二次修改

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -3,5 +3,8 @@ package com.test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("这是一个测试类");
+		for(int i=0;i<10;i++){
+			System.out.println("这是一个循环"+i);
+		}
 	}
 }
