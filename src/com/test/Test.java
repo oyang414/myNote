bd2179d131703fc1e07f374824b09e6c587d009c
oyang414 <o_yang1993@163.com>
2016-06-15 10:20:49 +0800

    啊哈

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -4,7 +4,9 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("这是一个测试类");
 		for(int i=0;i<10;i++){
-			System.out.println("这是一个循环"+i);
+			System.out.println("这是一个循环,循环第"+(i+1)+"次");
 		}
+		汽车 s = new 汽车();
+		System.out.println(s.高度);
 	}
 }
---
+++ src/com/test/汽车.java
@@ -0,0 +1,5 @@
+package com.test;
+
+public class 汽车 {
+	String 高度="1米";
+}
